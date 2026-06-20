d6c1660931c9befb6354fd0af667f53e1b0327d9
atharv-shewale <atharvshewale19@gmail.com>
2026-06-20 13:07:09 +0530

    Clean up project structure and remove extra scripts

--- HousingPricePrediction/download.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-url = 'https://raw.githubusercontent.com/YBI-Foundation/Dataset/main/Housing.csv'
-urllib.request.urlretrieve(url, 'Housing.csv')
-print("Downloaded successfully.")
